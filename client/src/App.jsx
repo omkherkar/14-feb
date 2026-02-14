8e354b8df2001bd8d9e5e24b158d27d9d92c8e51
om kherkar <kherkarvivek@gmail.com>
2026-02-14 21:22:09 +0530

    initia add

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <> 
+      <div class="bg-orange-500"></div>
+      <h1 class="bg-orange-500">hello</h1>
+    </>
+  )
+}
+
+export default App
